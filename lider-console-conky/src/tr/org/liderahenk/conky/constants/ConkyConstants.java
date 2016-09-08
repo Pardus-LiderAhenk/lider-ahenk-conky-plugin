b6afc116826dbf14caba81a83e9fd835eb6efd20
emrekgn <emre.akkaya@agem.com.tr>
2016-09-08 14:46:31 +0300

    add sample conky config files

--- lider-console-conky/src/tr/org/liderahenk/conky/constants/ConkyConstants.java
+++ lider-console-conky/src/tr/org/liderahenk/conky/constants/ConkyConstants.java
@@ -1,13 +1,17 @@
 package tr.org.liderahenk.conky.constants;
 
 public class ConkyConstants {
-	
+
 	public static final String PLUGIN_NAME = "conky";
-	
+
 	public static final String PLUGIN_VERSION = "1.0.0";
-	
+
 	public static final class PARAMETERS {
 		public static final String MESSAGE = "message";
 	}
-		
+
+	public static final class PLUGIN_ID {
+		public static final String CONKY = "lider-console-conky";
+	}
+
 }
