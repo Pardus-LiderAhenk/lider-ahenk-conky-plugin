faae075bf16e1cdcdf771d66e08578f7eb1f0868
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-25 16:02:00 +0300

    fix 1.0.0-beta version

--- lider-console-conky/src/tr/org/liderahenk/conky/constants/ConkyConstants.java
+++ lider-console-conky/src/tr/org/liderahenk/conky/constants/ConkyConstants.java
@@ -4,7 +4,7 @@ public class ConkyConstants {
 
 	public static final String PLUGIN_NAME = "conky";
 
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 
 	public static final class PARAMETERS {
 		public static final String MESSAGE = "message";
